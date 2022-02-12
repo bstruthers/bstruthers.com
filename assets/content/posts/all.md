22a3fca033a95c3ad4efe929b09adfbe5e8df367
Ben Struthers <ben@bstruthers.com>
2022-02-12 15:55:10 -0600

    update: remove nx-weblog assets and create a rough build script

--- assets/content/posts/all.md
+++ assets/content/posts/all.md
@@ -1,3 +1,9 @@
 # All Posts
 
+## Angular
+
+- [Writing an Angular+Markdown Blogging Framework. Part 1 of 4](/posts/Writing-an-Angular%2BMarkdown-Blogging-Framework_Part-1)
+
+## Misc.
+
 - [Hitting the reset button](/posts/hitting-the-reset-button)
---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Remove the existing distributable
+if [[ -d "dist" ]]; then
+    rm -rf dist
+fi
+
+# Copy in the latest build
+# TODO: pull from github release
+cp -r ../nx-weblog/dist/apps/blog dist
+
+# Copy latest assets
+cp -r assets dist/assests
+
+# Update the html with the meta tags, etc
+head -2 dist/index.html > 1
+tail -7 dist/index.html > 3
+cat 1 index.tmpl 3 > dist/index.html
+rm 1 3
